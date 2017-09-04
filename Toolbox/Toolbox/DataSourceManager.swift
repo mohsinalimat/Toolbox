46ee66278f2f9f5e2825493125d07afd5364e1e2
Light413 <18016373660@163.com>
2017-09-04 18:04:35 +0800

    m1.完善下载操作。2.下载队列归档处理。

--- Toolbox/Toolbox/DataSourceManager.swift
+++ Toolbox/Toolbox/DataSourceManager.swift
@@ -13,14 +13,25 @@ class DataSourceManager: NSObject {
 
     static let `default` = DataSourceManager()
     let subPathArr = [kpackage_info,ksync_manifest,ktdafactorymobilebaseline]
-    let infodatapath = LibraryPath.appending("/Application data")
     
+    var totalDownloadCnt:UInt8 = 0;
+    var currentDownloadCnt:UInt8 = 0;
+    let kLibrary_tmp_path = LibraryPath.appending("/TDLibrary/tmp")
+    let kPlistinfo_path = LibraryPath.appending("/Application data")
+    let kDownload_queue_path:String
+    
+    override init() {
+        FILESManager.default.fileExistsAt(path: kLibrary_tmp_path)
+        FILESManager.default.fileExistsAt(path: kPlistinfo_path)
+        kDownload_queue_path = kPlistinfo_path.appending("/downloadqueuelist.plist")
+    }
+    
+    
+    //MARK:-
     func checkupdateFromServer() {
-        FILESManager.default.fileExistsAt(path: infodatapath)
+        let semaphore = DispatchSemaphore.init(value: 1)
         for base in kDataSourceLocations {
-            let semaphore = DispatchSemaphore.init(value: 1)
             semaphore.wait()
-            
             let group = DispatchGroup.init()
             var package_info = [String:Any]()
 
@@ -44,20 +55,21 @@ class DataSourceManager: NSObject {
                 
                 semaphore.signal()
             })
-
         }
+       
         
+        print("ok!")
     }
     
     func compareJsonInfoFromLocal(_ url:String , info:[String:Any]) {
-        
+      guard info.keys.count == 3 else {return}
       if let ret = DataSourceModel.search(with: "location_url='\(url)'", orderBy: nil){
+        let m = ret.first as! DataSourceModel
+        guard let server_syncArr = info["sync_manifest.json"] as?[[String:String]] else {return}
+        guard let syncjsonStr = m.sync_manifest else{return}
+        
         if !ret.isEmpty && ret.count > 0{
             //比较同步信息
-            let m = ret.first as! DataSourceModel
-            guard let server_syncArr = info["sync_manifest.json"] as?[[String:String]] else {return}
-            guard let syncjsonStr = m.sync_manifest else{return}
-            
             do{
                 guard let data = syncjsonStr.data(using: String.Encoding.utf8) else{return}
                 guard let local_arr = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as? [[String:String]] else {return}
@@ -68,20 +80,18 @@ class DataSourceManager: NSObject {
                     for ldic in local_arr {
                        guard let doc_number_l = ldic["doc_number"] else{return}
                        guard let doc_version_l = ldic["revision_number"] else{return}
-                       if doc_number == doc_number_l && doc_version == doc_version_l{
-                        //下载新版本
+                       if doc_number == doc_number_l && doc_version == doc_version_l {/////////////////
+                        //添加到下载
                         let zip:String! = sdic["file_loc"]
-                        NSKeyedArchiver.archiveRootObject(zip, toFile: infodatapath.appending("/downloadqueuelist.plist"))
+                        let fileurl = url + "\(zip!)"
                         
+                        getPlistWith(filePath: fileurl)
                         }
                     }
                 }
             }catch{
                 print("JSONSerialization: \(error.localizedDescription)")
             }
-            
-
-            
         }else{
             let m = DataSourceModel()
             for(key,value) in info{
@@ -102,11 +112,108 @@ class DataSourceManager: NSObject {
             m.saveToDB();
             
             //添加到下载
+            for sdic in server_syncArr{
+                let zip:String! = sdic["file_loc"]
+                let fileurl = url + "\(zip!)"
+                getPlistWith(filePath: fileurl)
+            }
+        }
+      
+        //开始下载
+        startDownload()
+    }
+        
+    }
+    
+    func getPlistWith(filePath:String,isAdd:Bool = true) {
+        let fileurl = filePath
+        let plist = kDownload_queue_path
+        let old = NSKeyedUnarchiver.unarchiveObject(withFile: plist) as? [String]
+        var added = [String]();
+        
+        if let old = old {
+            added = added + old
+        }
+        if isAdd{
+            added.append(fileurl)
+        }else{
+            if added.contains(filePath) {
+                added.remove(at: added.index(of: filePath)!)
+            }
+        }
+
+        NSKeyedArchiver.archiveRootObject(added, toFile: plist)
+    }
+
+    func startDownload() {
+        let plist = kDownload_queue_path
+        let downloadfiles = NSKeyedUnarchiver.unarchiveObject(withFile: plist) as? [String]
+
+        let semaphore = DispatchSemaphore.init(value: 1)
+        //下载单个文件
+        func downloadFileFrom(path:URL?) {
+            print("download : \(path)")
+            guard let path = path else { return }
+            let downloadDestination : DownloadRequest.DownloadFileDestination = {_,_ in
+                let url = LibraryPath.appending("/TDLibrary/tmp").appending("/\(path.lastPathComponent)")
+                let des = URL (fileURLWithPath: url)
+                //let des:URL! = URL (string: url)//这个方法url文件无法写入？
+                return (des,[.removePreviousFile, .createIntermediateDirectories])
+            }
+            
+            Alamofire.download( path, to: downloadDestination).downloadProgress(queue: DispatchQueue.main) { (progress) in
+                print("\(progress.completedUnitCount) - \(progress.totalUnitCount)")
+                }.response { (response) in
+                    print("download single file ok.")
+                    let des = response.request?.url
+                    self.getPlistWith(filePath: "\(des!)", isAdd: false)
+                    self.currentDownloadCnt = self.currentDownloadCnt + 1
+                    semaphore.signal()
+            }
             
         }
         
-    }
+        if let downloadfiles = downloadfiles {
+            totalDownloadCnt = UInt8(downloadfiles.count)
+            
+            for url in downloadfiles {
+                semaphore.wait()
+                let u = URL (string: url)
+                downloadFileFrom(path: u)
+            
+            }
+            
+           print("download finished...")
 
+        //...解压文件
+            
+            
+        }
+        
+        
+        
     }
     
+    
+ 
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
