e7bbca45c13836925e66c90fb4eb8544e411cec3
Light413 <18016373660@163.com>
2017-10-13 01:38:11 +0800

    add panGenesture and target.

--- Toolbox/Toolbox/ViewSideController.swift
+++ Toolbox/Toolbox/ViewSideController.swift
@@ -12,6 +12,8 @@ class ViewSideController: BaseViewController {
 
     var isOpen:Bool = false
     
+    var view_init_center:CGPoint?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("ViewSideController: viewDidLoad")
@@ -28,7 +30,10 @@ class ViewSideController: BaseViewController {
         imgv.image =  UIImage (named: "illustrations_selected")
         imgv.isUserInteractionEnabled = true*/
         let tap = UITapGestureRecognizer.init(target: self, action: #selector(tapClick))
-        view.addGestureRecognizer(tap)
+        //view.addGestureRecognizer(tap)
+        
+        let panges = UIPanGestureRecognizer.init(target: self, action:#selector(panGestureAction(_ :)))
+        view.addGestureRecognizer(panges)
         
         let ig = UIImage (named: "illustrations_selected")
         let btn = UIButton (frame: CGRect (x: 0, y: (kCurrentScreenHight - 100 - 64 - 49)/2.0, width: 30, height: 100))
@@ -39,12 +44,64 @@ class ViewSideController: BaseViewController {
         let bgview = UIView (frame: CGRect (x: btn.frame.width, y: 0, width: view.frame.width - btn.frame.width, height: view.frame.height))
         bgview.backgroundColor = kTableviewBackgroundColor
 
-        //let _v = ImgDetailView (frame: CGRect (x: 20, y: 0, width: bgview.frame.width - 40, height: bgview.frame.height))
         let _v = Bundle.main.loadNibNamed("ImgDetailView", owner: nil, options: nil)?.last as! ImgDetailView
         _v.frame = CGRect (x: 20, y: 0, width: bgview.frame.width - 40, height: bgview.frame.height)
         _v.backgroundColor = UIColor.clear
         bgview.addSubview(_v)
-        view.addSubview(bgview)        
+        view.addSubview(bgview)
+
+        if view_init_center == nil{
+            print("view.center:\(view.center)")
+            view_init_center = view.center
+        }
+        
+    }
+    
+    
+    func panGestureAction(_ gesture:UIPanGestureRecognizer) {
+        let move_x = gesture.translation(in: view).x
+        let vx = gesture.velocity(in: view).x
+        
+        //print(vx)
+        
+        let left = (view_init_center?.x)! - view.center.x
+        if  left <= 405 && vx < 0 {
+            view.center = CGPoint.init(x: view.center.x + move_x, y: view.center.y)
+            if left < 300 {
+                UIView.beginAnimations(nil, context: nil)
+                view.center = CGPoint.init(x: (view_init_center?.x)! - CGFloat(405), y: view.center.y)
+                UIView.commitAnimations()
+                isOpen = true
+            }
+        }else if left > 0 && vx > 0 {
+            view.center = CGPoint.init(x: view.center.x + move_x, y: view.center.y)
+            if left < 300 {
+                UIView.beginAnimations(nil, context: nil)
+                view.center = view_init_center!
+                UIView.commitAnimations()
+                isOpen = false
+            }
+        }
+        
+        //1214  807
+        gesture.setTranslation(CGPoint.init(x: 0, y: 0), in: view)
+        if (gesture.state == .ended) {
+            print("end")
+            print(view.center)
+            let left = (view_init_center?.x)! - view.center.x
+            if left < 200 && vx < 0 {
+                UIView.beginAnimations(nil, context: nil)
+                view.center = view_init_center!
+                UIView.commitAnimations()
+                isOpen = false
+            }else if left > 200 && vx > 0 {
+                UIView.beginAnimations(nil, context: nil)
+                view.center = CGPoint.init(x: (view_init_center?.x)! - CGFloat(405), y: view.center.y)
+                UIView.commitAnimations()
+                isOpen = true
+            }
+        }
+        
     }
     
     
@@ -54,10 +111,14 @@ class ViewSideController: BaseViewController {
         
         if isOpen {
             isOpen = false
-            view.transform = CGAffineTransform (translationX: 0, y: 0)
+            UIView.beginAnimations(nil, context: nil)
+            view.center = view_init_center!
+            UIView.commitAnimations()
         }else{
             isOpen = true
-            view.transform = CGAffineTransform (translationX: -420, y: 0)
+            UIView.beginAnimations(nil, context: nil)
+            view.center = CGPoint.init(x: (view_init_center?.x)! - CGFloat(405), y: view.center.y)
+            UIView.commitAnimations()
         }
         
         
